eb6b2208308daa2f1e18e16ecbb05b7195ec0a9d
chris <sirleech@gmail.com>
2012-10-15 11:50:58 +1100

    merge change done locally

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-header('Content-Type: application/xml');
+header('Content-Type: text/html');
 header('Cache-Control: no-cache');
 header('Pragma: no-cache');
 header("Access-Control-Allow-Origin: *");
@@ -10,6 +10,31 @@ $url = 'ftp://ftp.bom.gov.au/anon/gen/fwo/IDN10035.xml';
 $xmlStr = file_get_contents($url);
 $xml = new SimpleXMLElement($xmlStr);
 
-echo $xml->asXML();
+//$result = $xml->amoc[0]->identifier;
+//echo $result;
+
+$result = $xml->forecast->area[1]->children()->children();
+echo $result;
+
+$result = $xml->xpath('/product/amoc');
+//echo $result->size;
+//var_dump($result);
+
+while(list( , $node) = each($result)) {
+    echo $node,"\n";
+}
+
+//$result = $xml->forecast->area[2]->children()->children();
+//echo "<br/>Forecast Code: $result";
+
+//$result = $xml->forecast->area[2]->children()->asXML();
+//echo "<br/>Max Temp: $result";
+
+//foreach ($xml->forecast->area[2]->children() as $element) {
+	//echo $element->asXML();
+//}
+
 
 ?>
+
+<a href="xmlproxy.php">raw xml</a>

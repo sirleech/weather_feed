6179b8efb7e7999a532e4d029ecab0f871fe9c94
Chris Lee <chrislee@bom.gov.au>
2013-11-06 13:34:27 +1100

    use attr names instead of xpath order for selection

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if ($product_id == "")
 
 $baseurl = 'ftp://ftp.bom.gov.au/anon/gen/fwo/';
 $url = $baseurl . $product_id . ".xml";
-echo $url;
+//echo $url;
 
 $xmlStr = file_get_contents($url);
 $xml = new SimpleXMLElement($xmlStr);
@@ -27,39 +27,62 @@ while(list( , $node) = each($result)) {
 
 ?>
 </h1>
-
+<h2>Today</h2>
+Forecast (short):
 <?php
 
-$result = $xml->xpath("//area[2]/forecast-period[1]/text[1]");
+$result = $xml->xpath("//area[3]/forecast-period[1]/text[@type='precis']");
 while(list( , $node) = each($result)) {
     echo $node;
 }
 
 ?>
 
-<br/>Max
+<br/>
+Forecast (long):
 <?php
 
-$result = $xml->xpath("//area[3]/forecast-period[1]/element[2]");
+$result = $xml->xpath("//area[2]/forecast-period[1]/text[@type='forecast']");
+while(list( , $node) = each($result)) {
+    echo $node;
+}
+
+?>
+
+<br/>Air temperature maximum: 
+<?php
+
+$result = $xml->xpath("//area[3]/forecast-period[1]/element[@type='air_temperature_maximum']");
 while(list( , $node) = each($result)) {
     echo $node;
 }
 
 ?>
 c
-<br/>Chance of Rain
+<br/>Chance of Rain:
 <?php
 
-$result = $xml->xpath("//area[3]/forecast-period[1]/text[2]");
+$result = $xml->xpath("//area[3]/forecast-period[1]/text[@type='probability_of_precipitation']");
 while(list( , $node) = each($result)) {
     echo $node;
 }
 
 ?>
-<br/>Icon Code
+
+<br/>Rainfall amount:
 <?php
 
-$result = $xml->xpath("//area[3]/forecast-period[1]/element[1]");
+$result = $xml->xpath("//area[3]/forecast-period[1]/element[@type='precipitation_range']");
+while(list( , $node) = each($result)) {
+    echo $node;
+}
+
+?>
+
+<br/>Forecast Icon Code:
+<?php
+
+$result = $xml->xpath("//area[3]/forecast-period[1]/element[@type='forecast_icon_code']");
 while(list( , $node) = each($result)) {
     echo $node;
 }
--- readme.md
+++ readme.md
@@ -3,8 +3,11 @@ http://www.bom.gov.au/act/forecasts/canberra.shtml --> "Product derived from IDN
 ftp://ftp.bom.gov.au/anon/gen/fwo/IDN10035.xml
 
 ## Capital Cities
-Canberra = IDN10035
-Melbourne Metro = IDV10450
+- Canberra = IDN10035
+- Melbourne = IDV10450
+- Brisbane = IDQ10095
+- Perth = IDW12300
+- Hobart = IDT13600
 
 ## Public Machine Readable Data
 - ftp://ftp.bom.gov.au/anon/gen/fwo/
@@ -16,9 +19,9 @@ http://www.bom.gov.au/NexGenFWS/icontable.shtml
 
 ~~~
 
-1 = Sunny
+1 = Sunny = http://www.bom.gov.au/images/symbols/large/sunny.png
 2 = Mostly Sunny
-3 = Mostly Sunny
+3 = Mostly Sunny/ Partly cloudy = http://www.bom.gov.au/images/symbols/large/partly-cloudy.png
 4 =
 5 =
 6 =
@@ -26,10 +29,11 @@ http://www.bom.gov.au/NexGenFWS/icontable.shtml
 8 =
 9 =
 10 =
-11 = Possible Shower/Shower or two
-12 = Rain/Rain at times
+11 = Possible Shower/Shower or two = http://www.bom.gov.au/images/symbols/large/showers.png
+12 = Rain/Rain at times = http://www.bom.gov.au/images/symbols/large/rain.png
 13 = 
-16 = Storm
+16 = Storm = http://www.bom.gov.au/images/symbols/large/storm.png
+17 = Light showers = http://www.bom.gov.au/images/symbols/large/light-showers.png
 
 
 ~~~
